34b0e73aed53a25c09d783e3b4088a57ea61a22f
JNF <jnfsmile@gmail.com>
2017-03-20 20:02:02 +0200

    Minor dev changes

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-rem npm run build
 copy server.js D:\Github\BeshviliProd /Y
 xcopy routes D:\Github\BeshviliProd\routes /D /I /Y /Q /E
 xcopy client\index.html D:\Github\BeshviliProd\client\index.html /D /I /Y /Q /E
