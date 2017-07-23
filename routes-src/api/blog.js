fadc113ff0c90e8fa06b014752f071aa5f8367ba
JNF <jnfsmile@gmail.com>
2017-07-23 21:57:45 +0300

    default blog post

--- routes-src/api/blog.js
+++ routes-src/api/blog.js
@@ -16,15 +16,42 @@ router.get('/blogs', function(req, res, next) {
 });
 /* GET One blog with the provided ID */
 router.get('/blog/:id', function(req, res, next) {
-  db.blogs.findOne({
-    _id: mongojs.ObjectId(req.params.id)
-  }, function(err, blogs) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(blogs);
-    }
-  });
+  let validId = typeof(id) !== "undefined" && (id.length == 12 || id.length == 24) && /^[0-9a-fA-F]+$/.test(id)
+  if (validId) {
+    db.blogs.findOne({
+      _id: mongojs.ObjectId(req.params.id)
+    }, function(err, blogs) {
+      if (err) {
+        console.log(`
+
+          ----------------------
+          ${err}
+          ----------------------
+
+          `)
+          res.send(err);
+      } else {
+        res.json(blogs);
+      }
+    });
+  } else {
+    db.blogs.find().limit(1).sort({$natural:-1}, function(err, blogs) {
+      if (err) {
+        console.log(`
+
+          ----------------------
+          ${err}
+          ----------------------
+
+          `)
+        res.send(err);
+      } else if (blogs.length == 0) {
+        res.send("no entry found");
+      } else {
+        res.json(blogs[0]);
+      }
+    });
+  }
 });
 
 module.exports = router;
